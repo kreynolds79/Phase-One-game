e647c5aaa6636115d0aba1ccb7db80d1aadf054a
Kyle <kr797918@ohio.edu>
2020-11-26 16:27:53 -0500

    Phase one commit
    hi :)

---
+++ samplemain.cc
@@ -0,0 +1,17 @@
+/*****************************************************
+This is a sample of what the main should like for the first phase of the
+Checkers game.
+	John Dolan	Ohio University		Fall 2020
+*************************************************************/
+#include<iostream>
+#include "game.h"
+#include "game.cc"
+using namespace std;
+using namespace main_savitch_14;
+
+
+int main(){
+    checkers mygame;
+    mygame.play();
+    return 0;
+}
